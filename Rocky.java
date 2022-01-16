a004bde043aca1230505185058a57516dd6d8ac2
Gabriel Racz <gabriel.s.racz@gmail.com>
2022-01-16 17:38:38 -0500

    file restructure

--- Rocky.java
+++
@@ -1,6 +0,0 @@
-public class Rocky extends Movable{
-
-    public Rocky(double initX, double initY, double initMass, double initRadius, double initXVelocity, double initYVelocity){
-        super(initX, initY, initMass, initRadius, initXVelocity, initYVelocity);
-    }
-}
